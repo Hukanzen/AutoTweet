a2bcf4382a8cba04618034c0216509b3b08aa6c0
Hukanzen <qazxswed1934@gmail.com>
2016-09-21 16:38:08 +0900

    3rd Commit

---
+++ mecab2/get_tweet.php
@@ -0,0 +1,60 @@
+<?php
+	require dirname(__FILE__).'/../TwistOAuth.phar'   ;
+	require dirname(__FILE__).'/../main/user.php'     ;
+	require dirname(__FILE__).'/../main/try_catch.php';
+	require dirname(__FILE__).'/func.php'   ;
+
+	require dirname(__FILE__).'/mysqli_connection.php'   ;
+
+	$to=new TwistOAuth($cK,$cS,$aT,$aTS);
+	$mecab=new MeCab_Tagger();
+
+	#$data=$to->get('statuses/home_timeline',array('count' => 100,'trim_user'=>false,'exclude_replies'=>false,'contributor_details'=>false,'include_entities'=>false));
+	$data=$to->get('statuses/user_timeline',array('screen_name'=>'@HHHalice','count' => 1,'trim_user'=>false,'exclude_replies'=>false,'contributor_details'=>false,'include_entities'=>false));
+
+
+	$link=db_connect();
+	
+	foreach($data as $key => $value){
+		$text=$value->text;
+//		var_dump($value);
+		if(!preg_match("/@/",$text)){
+			$text=mb_ereg_replace("\\\\","￥",$text);
+			$text=mysqli_real_escape_string($link,$text);
+			$sql="INSERT INTO Tweet(Data) VALUES(\"".$text."\")";
+			echo $sql."\n";
+			db_query($sql,$link);
+
+			$nodes=$mecab->parseToNode($text);
+			
+			$i=0;
+			$junban=array(-1,-1,-1);
+			foreach($nodes as $n){
+				table_check_create($link,$n->getId());
+			//	if(($n->getId())<=68){
+					$word=$n->getSurface();
+					$word=mb_ereg_replace("\\\\","￥",$word);
+					$word=mysqli_real_escape_string($link,$word);
+					$sql="INSERT INTO POSpeech_".$n->getId()."_db(Data) VALUES(\"".$word."\");";
+					db_query($sql,$link);
+					$junban[$i%3]=$n->getId();
+					if($i==2){
+						$sql="INSERT INTO Junban(j0,j1,j2) VALUES("."-1".",".$junban[1].",".$junban[2].");";
+						db_query($sql,$link);
+						$junban=array(-1,-1,-1);
+					}elseif($i!=0 && !($i%3)){
+						$sql="INSERT INTO Junban(j0,j1,j2) VALUES(".$junban[0].",".$junban[1].",".$junban[2].");";
+						db_query($sql,$link);
+						$junban=array(-1,-1,-1);
+					}
+					$i++;
+			//	}
+			}
+		}else{
+			continue;
+		}
+	}
+
+	db_close($link);	
+//	var_dump($data);
+?>
---
+++ mecab2/tweet.php
@@ -0,0 +1,84 @@
+<?php 
+	require dirname(__FILE__).'/../TwistOAuth.phar'   ;
+	require dirname(__FILE__).'/../main/user.php'     ;
+	require dirname(__FILE__).'/../main/try_catch.php';
+	require dirname(__FILE__).'//func.php'   ;
+
+	require dirname(__FILE__).'/mysqli_connection.php'   ;
+	
+	$ln=db_connect();
+
+	$pattern=array();
+	
+	$sql=array("SELECT * FROM mecab2.Junban WHERE j0 = -1");
+	$aRES=db_a_fetch_assoc($sql,$ln);
+	$count=count($aRES[0])-1;
+
+	$seed=rand(0,$count);
+
+	array_push($pattern,$aRES[0][$seed]['j0']);
+	array_push($pattern,$aRES[0][$seed]['j1']);
+	array_push($pattern,$aRES[0][$seed]['j2']);
+	
+//	var_dump($aRES);
+	//for($i=0;$i<10;$i++){
+	while(1){
+		//echo "seed".$seed."\n";
+		print_r($aRES[0]);
+		$sql=array("SELECT * FROM mecab2.Junban WHERE j0 = ".$aRES[0][$seed]['j2'].";");
+		/* $a[RES][0][$seed]['j2']の値がうまく、j0にない場合、エラーで終了 */
+//		echo $sql[0];
+		$aRES=db_a_fetch_assoc($sql,$ln);
+		$count=count($aRES[0])-1;
+
+//		RETRY_seed1:
+		$seed=rand(0,$count);
+//		if((($aRES[0][$seed]['j2'])===''&&($aRES[0][$seed]['j2']) != 0) || (($aRES[0][$seed]['j2'])===null &&($aRES[0][$seed]['j2']) != 0 )){
+//			goto RETRY_seed1;
+//		}
+		echo $seed;
+		array_push($pattern,$aRES[0][$seed]['j0']);
+		array_push($pattern,$aRES[0][$seed]['j1']);
+		array_push($pattern,$aRES[0][$seed]['j2']);
+//	echo "id=".$aRES[0][$seed]['ID']."\n";
+
+		if($aRES[0][$seed]['j1']==-1 || $aRES[0][$seed]['j2']==-1){
+			break;
+		}
+	}
+	
+//	var_dump($pattern);
+	make_tweet:
+	$tweet_data="#自作自動エロツイート生成bot\n";
+	for($i=0;$i<rand(1,5);$i++){
+		foreach($pattern as $key => $value){
+			if($value == -1)	continue;
+			elseif($key == 0)     continue;
+			$sql=array("SELECT * FROM mecab2.POSpeech_".$value."_db;");
+			$aRES=db_a_fetch_assoc($sql,$ln);
+			$count=count($aRES[0]);
+
+	//		RETRY_text:
+			$text=' ';
+			$text=$aRES[0][rand(0,$count)]['Data'];
+	//		if((($text)===''&&($text) != 0) || (($text)===null &&($text) != 0 )){
+	//			goto RETRY_text;
+	//		}
+
+
+			$text=mb_ereg_replace("￥￥￥￥","\\\\",$text);
+			$tweet_data.=$text;
+			//if(preg_match(
+		}
+		$tweet_data.="。 ";
+	}
+	
+	if(mb_strlen($tweet_data) >140){
+		goto make_tweet;
+	}
+	
+	db_close($ln);
+
+//	echo $tweet_data;
+	try_catch($cK,$cS,$aT,$aTS,$tweet_data);
+?>
